a327ca5c4640563cd3b0a18e16fd998c2e003d98
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2025-01-22 20:27:26 -0500

    a

--- worlds/fridaynightfunkin/__init__.py
+++ worlds/fridaynightfunkin/__init__.py
@@ -7,7 +7,13 @@ from typing import List, ClassVar, Type, Tuple
 from worlds.AutoWorld import World, WebWorld
 from math import floor
 from Options import PerGameCommonOptions
+import random
+import re
 
+from .ModHandler import (
+    get_player_specific_ids,
+    extract_mod_data,
+)
 from .Items import FunkinItem, FunkinFixedItem
 from .Locations import FunkinLocation
 from .Options import *
@@ -47,11 +53,12 @@ class FunkinWorld(World):
     origin_region_name = "Freeplay"
 
     victory_song_name: str = ""
+    victory_song_id: int
     location_count: int
     songList: List[str]
     excludedSongs: List[str]
 
-    fnfUtil = FunkinUtils()
+    fnfUtil = FunkinUtils(extract_mod_data)
     filler_item_names = list(fnfUtil.filler_item_weights.keys())
     filler_item_weights = list(fnfUtil.filler_item_weights.values())
     item_name_to_id = {name: code for name, code in fnfUtil.item_names_to_id.items()}
@@ -97,10 +104,25 @@ class FunkinWorld(World):
         self.ticket_win_percentage = self.options.ticket_win_percentage.value
         self.graderequirement = self.options.graderequirement.value
         self.accrequirement = self.options.accrequirement.value
-        #self.e_weight = self.options.trapAmount.value later
-        #self.fnfUtil.initSongItems()
-        available_song_keys = self.fnfUtil.get_songs_map()
-        self.create_song_pool(available_song_keys)
+        #self.e_weight = self.options.trapAmount.value imma do this later
+        while True:
+            # In most cases this should only need to run once
+            available_song_keys, song_ids = self.fnfUtil.get_songs_with_settings(self.options.mods_enabled.value, get_player_specific_ids(self.options.songList.value, self.fnfUtil.song_items))
+
+            print(get_player_specific_ids(self.options.songList.value, self.fnfUtil.song_items))
+            # Choose victory song from current available keys, so we can access the song id
+            chosen_song_index = random.randrange(0, len(available_song_keys))
+            self.victory_song_name = available_song_keys[chosen_song_index]
+            self.victory_song_id = int(song_ids[chosen_song_index])
+            del available_song_keys[chosen_song_index]
+
+            available_song_keys = available_song_keys
+            count_needed_for_start = max(0, 1)
+            if len(available_song_keys) + 1 >= count_needed_for_start + 11:
+                final_song_list = available_song_keys
+                break
+
+        self.create_song_pool(final_song_list)
 
     def create_item(self, name: str) -> Item:
         if name == self.fnfUtil.SHOW_TICKET_NAME:
@@ -115,17 +137,19 @@ class FunkinWorld(World):
         if trap:
             return FunkinFixedItem(name, ItemClassification.trap, trap, self.player)
 
-        print("Song list for " + self.player_name + " is " + str(self.options.songList.value))
+        # print("Song list for " + self.player_name + " is " + str(self.options.songList.value))
 
         song = self.fnfUtil.song_items.get(name)
-
+        print(str(self.player_name) + ": " + str(song.songName))
         return FunkinItem(name, self.player, song)
 
-    def create_items(self) -> None:
+    '''def create_items(self) -> None:
         self.itemPool = self.multiworld.itempool
         self.player_songs = self.options.songList.value
         self.playable_songs = []
         self.junk = []
+        item_count = self.get_ticket_count()
+
 
         for song in self.multiworld.itempool:
             for songs in self.player_songs:
@@ -137,9 +161,51 @@ class FunkinWorld(World):
                 self.junk.append(self.create_item(self.playable_songs[i]))
 
 
-        self.multiworld.itempool = self.playable_songs
+        self.multiworld.itempool += self.playable_songs
         for junk in self.junk:
             self.multiworld.itempool.append(junk)
+
+        item_count += len(self.songList)
+        # Then add all traps, making sure we don't over fill
+        trap_count = min(self.location_count - item_count, self.get_trap_count())
+        trap_list = self.get_available_traps()
+        if len(trap_list) > 0 and trap_count > 0:
+            for _ in range(0, trap_count):
+                index = self.random.randrange(0, len(trap_list))
+                self.multiworld.itempool.append(self.create_item(trap_list[index]))
+
+            item_count += trap_count
+
+        items_left = self.location_count - item_count
+        if items_left <= 0:
+            return
+
+        # When it comes to filling remaining spaces, we have 2 options. A useless filler or additional songs.
+        # First fill 50% with the filler. The rest is to be duplicate songs.
+        filler_count = floor(0.5 * items_left)
+        items_left -= filler_count
+
+        for _ in range(0, filler_count):
+            self.multiworld.itempool.append(self.create_item(self.get_filler_item_name()))
+
+        # All remaining spots are filled with duplicate songs. Duplicates are set to useful instead of progression
+        # to cut down on the number of progression items that Muse Dash puts into the pool.
+
+        # This is for the extraordinary case of needing to fill a lot of items.
+        while items_left > len(song_keys_in_pool):
+            for key in song_keys_in_pool:
+                item = self.create_item(key)
+                item.classification = ItemClassification.useful
+                self.multiworld.itempool.append(item)
+            items_left -= 1
+
+        # Otherwise add a random assortment of songs
+        self.random.shuffle(song_keys_in_pool)
+        for i in range(0, len(song_keys_in_pool)):
+            item = self.create_item(song_keys_in_pool[i])
+            item.classification = ItemClassification.useful
+            self.multiworld.itempool.append(item)'''
+
 
 
     def create_event(self, event: str) -> Item:
@@ -207,7 +273,7 @@ class FunkinWorld(World):
         menu_region = Region("Freeplay", self.player, self.multiworld)
         self.multiworld.regions += [menu_region]
 
-        all_selected_locations:List[str] = self.fnfUtil.get_songs_map().copy()
+        all_selected_locations:List[str] = self.songList.copy()
         self.random.shuffle(all_selected_locations)
 
         # Adds 1 item locations per song to the menu region.
@@ -221,43 +287,43 @@ class FunkinWorld(World):
         startingSong = self.fnfUtil.get_songs_map()[self.random.randrange(0, len(self.fnfUtil.get_songs_map()))]
         FNFBaseList.localSongList.remove(startingSong)
         self.multiworld.push_precollected(self.create_item(startingSong))
+        self.songList = available_song_keys
 
         if self.options.starting_song.value != "":
             starting_song_count = 1
         else:
             starting_song_count = 0
         self.random.shuffle(available_song_keys)
-        song_count = len(self.fnfUtil.get_songs_map())
+        song_count = len(self.songList)
         # choose a random victory song from the available songs
         chosen_song = self.random.randrange(0, len(available_song_keys))
         if chosen_song < song_count:
-            self.victory_song_name = self.fnfUtil.get_songs_map()[chosen_song]
-            del self.fnfUtil.get_songs_map()[chosen_song]
+            self.victory_song_name = self.songList[chosen_song]
+            del self.songList[chosen_song]
         else:
             self.victory_song_name = available_song_keys[chosen_song - song_count]
             del available_song_keys[chosen_song - song_count]
 
         # Then attempt to fulfill any remaining songs for interim songs
-        if len(self.fnfUtil.get_songs_map()) < song_count:
-            for _ in range(0, len(self.fnfUtil.get_songs_map())):
+        if len(self.songList) < song_count:
+            for _ in range(0, len(self.songList)):
                 if len(available_song_keys) <= 0:
                     break
-                self.fnfUtil.get_songs_map().append(available_song_keys.pop())
+                self.songList.append(available_song_keys.pop())
 
-        self.location_count = len(self.fnfUtil.get_songs_map())
+        self.location_count = len(self.songList)
 
     def create_items(self) -> None:
-        song_keys_in_pool = self.fnfUtil.get_songs_map().copy()
         item_count = self.get_ticket_count()
 
         #I'll figure this out eventually
         # First add all goal song tokens
-        '''for _ in range(0, item_count):
-            self.multiworld.itempool.append(self.create_item(self.fnfUtil.SHOW_TICKET_NAME))'''
+        for _ in range(0, item_count):
+            self.multiworld.itempool.append(self.create_item(self.fnfUtil.SHOW_TICKET_NAME))
 
         # Then add 1 copy of every song
-        item_count += len(self.fnfUtil.get_songs_map())
-        for song in self.fnfUtil.get_songs_map():
+        item_count += len(self.songList)
+        for song in self.songList:
             self.multiworld.itempool.append(self.create_item(song))
 
         # Then add all traps, making sure we don't over fill
@@ -280,26 +346,27 @@ class FunkinWorld(World):
         filler_count = floor(0.5 * items_left)
         items_left -= filler_count
 
-        for _ in range(0, filler_count):
-            self.multiworld.itempool.append(self.create_item(self.get_filler_item_name()))
-
         # All remaining spots are filled with duplicate songs. Duplicates are set to useful instead of progression
         # to cut down on the number of progression items that Muse Dash puts into the pool.
 
         # This is for the extraordinary case of needing to fill a lot of items.
-        while items_left > len(song_keys_in_pool):
-            for key in song_keys_in_pool:
+        while items_left > len(self.songList):
+            for key in self.songList:
                 item = self.create_item(key)
                 item.classification = ItemClassification.useful
                 self.multiworld.itempool.append(item)
             items_left -= 1
 
         # Otherwise add a random assortment of songs
-        self.random.shuffle(song_keys_in_pool)
-        for i in range(0, len(song_keys_in_pool)):
-            item = self.create_item(song_keys_in_pool[i])
+        self.random.shuffle(self.songList)
+        for i in range(0, len(self.songList)):
+            item = self.create_item(self.songList[i])
             item.classification = ItemClassification.useful
             self.multiworld.itempool.append(item)
+
+        items_left2 = self.location_count - items_left
+        for _ in range(0, items_left2):
+            self.multiworld.itempool.append(self.create_item(self.fnfUtil.SHOW_TICKET_NAME))
     #
     # def pre_fill(self) -> None:
     #
@@ -311,7 +378,7 @@ class FunkinWorld(World):
     #               progitempool: List["Item"],
     #               usefulitempool: List["Item"],
     #               filleritempool: List["Item"],
-    #               fill_locations: List["Location"]) -> None:
+    #               fill_locations: List["Location"]) -> None:'''
 
     def set_rules(self) -> None:
         '''self.multiworld.completion_condition[self.player] = lambda state: \
@@ -331,7 +398,7 @@ class FunkinWorld(World):
                 if location == song and not song.player == self.player:
                      self.multiworld.itempool[location].classification = ItemClassification.filler
 
-        print(self.multiworld.exclude_locations)
+        # print(self.multiworld.exclude_locations)
 
 
     def get_trap_count(self) -> int:
@@ -354,6 +421,7 @@ class FunkinWorld(World):
             "deathLink": self.options.deathlink.value,
             "fullSongCount": len(self.fnfUtil.get_songs_map()),
             "victoryLocation": self.victory_song_name,
+            "victoryID": self.victory_song_id,
             "ticketWinCount": self.get_ticket_win_count(),
             "gradeNeeded": self.options.graderequirement.value,
             "accuracyNeeded": self.options.accrequirement.value,
